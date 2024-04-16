a2ea12d041ab9cb4bc789ae76fcb332f4748ca9e
MayurPatel0 <72008963+MayurPatel0@users.noreply.github.com>
2024-04-16 12:58:49 +0100

    Updated!

--- app/src/main/java/com/example/component/ui/query/QueryFragment.kt
+++ app/src/main/java/com/example/component/ui/query/QueryFragment.kt
@@ -1,12 +1,16 @@
 package com.example.component.ui.query
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.component.R
 import com.example.component.databinding.FragmentQueryBinding
 
 class QueryFragment : Fragment() {
@@ -16,6 +20,8 @@ class QueryFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var imagePick: ImageView
+    private lateinit var uploadPick: CardView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,9 +35,34 @@ class QueryFragment : Fragment() {
         val root: View = binding.root
 
 
+        uploadPick = root.findViewById(R.id.imagePicker)
+
+        uploadPick.setOnClickListener {
+            imagePick = root.findViewById(R.id.imagePick)
+            uploadImage()
+        }
 
         return root
     }
+
+    private fun uploadImage() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_IMAGE_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CODE && resultCode == RESULT_OK) {
+            val imageUri = data?.data ?: return
+            imagePick.setImageURI(imageUri)
+        }
+    }
+
+    companion object {
+        private const val REQUEST_IMAGE_CODE = 100
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
