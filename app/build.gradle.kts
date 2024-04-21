e7b53769e14da82e629224edb77c3ed3a0fd11ce
MayurPatel0 <72008963+MayurPatel0@users.noreply.github.com>
2024-04-21 03:30:41 +0100

    Updated!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+
     implementation (libs.material)
 
 
--- app/src/main/java/com/example/component/ui/query/QueryFragment.kt
+++ app/src/main/java/com/example/component/ui/query/QueryFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.component.R
 import com.example.component.databinding.FragmentQueryBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
@@ -27,13 +28,12 @@ class QueryFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
     private lateinit var imagePick: ImageView
-    private lateinit var uploadPick: CardView
 
     private lateinit var queryDatabase: FirebaseFirestore
     private lateinit var emailID: TextInputEditText
     private lateinit var queryText: TextInputEditText
     private lateinit var queryImageStorage: FirebaseStorage
-    private var imageUri: Uri? = null
+    private var imageUrl: String? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -44,23 +44,39 @@ class QueryFragment : Fragment() {
         _binding = FragmentQueryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        emailID = root.findViewById(R.id.email)
+        queryText = root.findViewById(R.id.feedback)
         queryDatabase = FirebaseFirestore.getInstance()
         queryImageStorage = FirebaseStorage.getInstance()
 
-        uploadPick = root.findViewById(R.id.imagePicker)
-
-        uploadPick.setOnClickListener {
-            imagePick = root.findViewById(R.id.imagePick)
+        val imageChooser = root.findViewById<Button>(R.id.chooseImage)
+        imageChooser.setOnClickListener {
             uploadImage()
         }
 
         val submitBtn = root.findViewById<Button>(R.id.submitQuery)
 
         submitBtn.setOnClickListener {
-            uploadStorage()
+            val queryEmail = emailID.text.toString()
+            val query = queryText.text.toString()
+
+            if (queryEmail.isEmpty() && query.isEmpty()) {
+                cannotSubmit()
+            }
+            else {
+                uploadStorage()
+            }
         }
 
         return root
+    }
+
+    private fun cannotSubmit() {
+        MaterialAlertDialogBuilder(requireActivity())
+            .setTitle("Unable to Submit QUERY!!")
+            .setMessage("Please make sure that you have filled the email and query text fields, as they are required and you wouldn't be able to submit this query.")
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 
 
@@ -70,23 +86,40 @@ class QueryFragment : Fragment() {
         startActivityForResult(intent, REQUEST_IMAGE_CODE)
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_IMAGE_CODE && resultCode == RESULT_OK) {
             val imageUri = data?.data ?: return
+            imageUrl = imageUri.toString()
             imagePick.setImageURI(imageUri)
         }
     }
 
     private fun uploadStorage() {
-        val image = queryImageStorage.reference.child("Images/${UUID.randomUUID()}").putFile(imageUri!!)
+        val image = queryImageStorage.reference.child("Images/" + UUID.randomUUID().toString())
+        val uploadTask = image.putFile(Uri.parse(imageUrl!!))
 
-        image.addOnSuccessListener { task ->
-            task.storage.downloadUrl.addOnSuccessListener { uri ->
-                queryFirestore(uri.toString())
+        uploadTask.addOnSuccessListener {
+            image.downloadUrl.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUrl = task.result.toString()
+                    queryFirestore(downloadUrl)
+                }
+                else {
+                    uploadFail()
+                }
             }
         }
 
+    }
+
+    private fun uploadFail() {
+        MaterialAlertDialogBuilder(requireActivity())
+            .setTitle("Image Upload Failure!!")
+            .setMessage("Please try again or alternatively email us your query directly on test123@123.com.")
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 
     private fun queryFirestore(queryImgURL: String) {
@@ -105,6 +138,7 @@ class QueryFragment : Fragment() {
             .addOnSuccessListener {
                 emailID.setText("")
                 queryText.setText("")
+                imageUrl = null
             }
 
             .addOnFailureListener {
